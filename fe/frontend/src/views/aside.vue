db475ad781c3fd66e3208e6d1858a39c7821a1af
fei <184918308@qq.com>
2023-04-02 10:44:17 +0800

    menu2

--- fe/frontend/src/views/aside.vue
+++ fe/frontend/src/views/aside.vue
@@ -1,17 +1,27 @@
 <template>
-<el-menu class="el-menu-vertical-demo" ref="menus" :default-openeds="['1']" @close="handleClose" :default-active="$route.path" router=true background-color="#D3DCE6" mode="horizontal">
-  <el-submenu index="1">
-      <template slot="title"><i class="el-icon-menu"></i>导航二</template>
+<el-menu router=true background-color="#D3DCE6" mode="horizontal" class="menuAlign">
         <el-menu-item-group>
-          <template slot="title">分组一</template>
-          <el-menu-item index="/decoration">House Decoration</el-menu-item>
-          <el-menu-item index="/post">Post</el-menu-item>
-          <el-menu-item index="/articles">Blog Posts</el-menu-item>
+          <template slot="title">
+            <i class="el-icon-menu"></i>
+            <span style="font-size:15px">Group1</span>
+          </template>
+          <el-menu-item index="/decoration">
+          <span style="margin-left:10px">House Decoration</span>
+          </el-menu-item>
         </el-menu-item-group>
-        <el-menu-item-group title="分组2">
-          <el-menu-item index="dd">选项3</el-menu-item>
+
+        <el-menu-item-group>
+          <template slot="title">
+          <i class="el-icon-menu"></i>
+            <span style="font-size:15px">Group2</span>
+          </template>
+          <el-menu-item index="/post">
+          <span style="margin-left:10px">Post</span>
+          </el-menu-item>
+          <el-menu-item index="/articles">
+          <span style="margin-left:10px">Blog Posts</span>
+          </el-menu-item>
         </el-menu-item-group>
-  </el-submenu>
 </el-menu>
 </template>
 
@@ -19,14 +29,27 @@
 export default {
   name: 'Aside',
   methods: {
+
+  handleOpen(key, keyPath) 
+  {
+    this.$refs.menus.open(keyPath);
+  },
   handleClose(key, keyPath) 
   {
     this.$refs.menus.open(keyPath);
-  }
+  },
+
+
   }
 } 
 
 </script>
 
 <style>
+
+  .menuAlign{
+    text-align:left;
+    margin: 0;
+    color: #303133;
+  }
 </style>
